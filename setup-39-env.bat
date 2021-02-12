10b562d39117a27791488c6fa7b96e3f334f35fa
Kyle King <kking@meso-scale.com>
2021-02-12 10:55:02 -0500

    test: generate HTML coverage report

--- setup-39-env.bat
+++ setup-39-env.bat
@@ -6,5 +6,6 @@ call .\env-3.9\Scripts\activate
 pip install wheel==0.36.2
 pip install pycryptodome==3.9.7
 pip install nose==1.3.7
+pip install coverage==5.4
 pip install -e .
-ECHO Test with: nosetests src\simplecrypt\tests.py
+ECHO Test with: nosetests src\simplecrypt\tests.py --stop --verbose --with-coverage --cover-package=simplecrypt --cover-html --cover-html-dir=cov_html
